d14fe38d975426973f9d0181639b6efbb40eccb6
Matt Wynne <matt@mattwynne.net>
2018-09-08 18:42:55 +0000

    Take the logic from https://gist.github.com/savetheclocktower/9b5f67c20f6c04e65ed88f2e594d43c1
    Seems simpler. Seems to work more reliably.

--- index.js
+++ index.js
@@ -14,44 +14,43 @@ function RotaryEncoder(pinA, pinB) {
 	this.gpioA = new Gpio(pinA, 'in', 'both');
 	this.gpioB = new Gpio(pinB, 'in', 'both');
 
-	this.a = 2;
-	this.b = 2;
+	this.a = 0;
+	this.b = 0;
+	this.last = null;
 
 	this.gpioA.watch((err, value) => {
 		if (err) {
 			this.emit('error', err);
-
 			return;
 		}
-
+		if (this.last === 'a')
+			return
+		this.last = 'a'
 		this.a = value;
+		this.tick(1);
 	});
 
 	this.gpioB.watch((err, value) => {
 		if (err) {
 			this.emit('error', err);
-
 			return;
 		}
-
+		if (this.last === 'b')
+			return
+		this.last = 'b'
 		this.b = value;
-
-		this.tick();
+		this.tick(-1);
 	});
 }
 
 RotaryEncoder.prototype = EventEmitter.prototype;
 
-RotaryEncoder.prototype.tick = function tick() {
+RotaryEncoder.prototype.tick = function tick(direction) {
 	const { a, b } = this;
 
-	if (a === 0 && b === 0 || a === 1 && b === 1) {
-		this.emit('rotation', 1);
-	} else if (a === 1 && b === 0 || a === 0 && b === 1 || a === 2 && b === 0) {
-		this.emit('rotation', -1);
+	if (a === 1 && b === 1) {
+		this.emit('rotation', direction);
 	}
-
-	return this;
 };
 
 module.exports = function rotaryEncoder(pinA, pinB) {
